80540462a18f2b5db55bcf3e824455e134adddde
Ansh Kushwaha <ansh.kushwaha26@gmail.com>
2023-10-15 13:23:14 +0530

    Progress 1 Hour Mark

--- src/main/java/com/project/contentcalendar/Application.java
+++ src/main/java/com/project/contentcalendar/Application.java
@@ -2,12 +2,16 @@ package com.project.contentcalendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-public class ContentCalendarApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContentCalendarApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ src/main/java/com/project/contentcalendar/controller/ContentController.java
@@ -0,0 +1,11 @@
+package com.project.contentcalendar.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/content")
+public class ContentController {
+
+
+}
